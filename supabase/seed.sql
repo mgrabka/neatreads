147989c884983bc09b53f8bbba1156d347b36791
Maksymilian Grabka <maks.grabka@icloud.com>
2023-09-14 21:19:59 +0200

    feat(database): add seed.sql data for testing purposes

---
+++ supabase/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO "auth"."users" ("id") VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'), ('b0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12'), ('c0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13');
+
+INSERT INTO library.ratings (book_id, user_id, rating, created_at, updated_at) VALUES
+('zaRoX10_UsMC', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 4, NOW(), NOW()),
+('RJxWIQOvoZUC', 'b0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 5, NOW(), NOW()),
+('_ojXNuzgHRcC', 'c0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 3, NOW(), NOW());
+
+INSERT INTO library.comments (rating_id, body, created_at, updated_at) VALUES
+(1, 'Great book. I really enjoyed it!', NOW(), NOW()),
+(2, 'Fantastic read. Highly recommended!', NOW(), NOW()),
+(3, 'It was okay. Not my favorite.', NOW(), NOW());
