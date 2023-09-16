488aa0baa66b10ec2a9c724343ea5b0f0d2d11ee
Maksymilian Grabka <maks.grabka@icloud.com>
2023-09-16 11:31:02 +0200

    feat(database): move all tables to public schema

--- supabase/migrations/20230914064038_initial_db_setup.sql
+++ supabase/migrations/20230914064038_initial_db_setup.sql
@@ -1,8 +1,4 @@
-CREATE SCHEMA library;
-
-GRANT USAGE ON SCHEMA library TO PUBLIC;
-
-CREATE TABLE library.ratings (
+CREATE TABLE ratings (
     id serial PRIMARY KEY,
     book_id varchar(20) NOT NULL,
     user_id uuid NOT NULL REFERENCES auth.users(id),
@@ -12,43 +8,43 @@ CREATE TABLE library.ratings (
     UNIQUE(user_id, book_id) 
 );
 
-CREATE INDEX idx_ratings_book_id ON library.ratings(book_id);
-CREATE INDEX idx_ratings_user_id ON library.ratings(user_id);
+CREATE INDEX idx_ratings_book_id ON ratings(book_id);
+CREATE INDEX idx_ratings_user_id ON ratings(user_id);
 
-ALTER TABLE library.ratings ENABLE ROW LEVEL SECURITY;
+ALTER TABLE ratings ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY insert_rating ON library.ratings FOR INSERT TO authenticated WITH CHECK (true);
-CREATE POLICY update_own_rating ON library.ratings FOR UPDATE USING (auth.uid() = user_id);
-CREATE POLICY delete_own_rating ON library.ratings FOR DELETE USING (auth.uid() = user_id);
-CREATE POLICY select_all_ratings ON library.ratings FOR SELECT USING (true);
+CREATE POLICY insert_rating ON ratings FOR INSERT TO authenticated WITH CHECK (true);
+CREATE POLICY update_own_rating ON ratings FOR UPDATE USING (auth.uid() = user_id);
+CREATE POLICY delete_own_rating ON ratings FOR DELETE USING (auth.uid() = user_id);
+CREATE POLICY select_all_ratings ON ratings FOR SELECT USING (true);
 
-GRANT SELECT ON TABLE library.ratings TO PUBLIC;
-GRANT INSERT, UPDATE, DELETE ON TABLE library.ratings TO authenticated;
+GRANT SELECT ON TABLE ratings TO PUBLIC;
+GRANT INSERT, UPDATE, DELETE ON TABLE ratings TO authenticated;
 
-CREATE TABLE library.comments (
+CREATE TABLE comments (
     id serial PRIMARY KEY,
-    rating_id bigint NOT NULL REFERENCES library.ratings(id),
+    rating_id bigint NOT NULL REFERENCES ratings(id),
     body text NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT now(),
     updated_at timestamp with time zone NOT NULL DEFAULT now(),
     UNIQUE(rating_id)
 );
 
-CREATE INDEX idx_comments_rating_id ON library.comments(rating_id);
+CREATE INDEX idx_comments_rating_id ON comments(rating_id);
 
-ALTER TABLE library.comments ENABLE ROW LEVEL SECURITY;
+ALTER TABLE comments ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY insert_comment ON library.comments FOR INSERT TO authenticated WITH CHECK (true);
-CREATE POLICY update_own_comment ON library.comments FOR UPDATE USING (auth.uid() = (SELECT user_id FROM library.ratings WHERE id = rating_id));
-CREATE POLICY delete_own_comment ON library.comments FOR DELETE USING (auth.uid() = (SELECT user_id FROM library.ratings WHERE id = rating_id));
-CREATE POLICY select_all_comments ON library.comments FOR SELECT USING (true);
+CREATE POLICY insert_comment ON comments FOR INSERT TO authenticated WITH CHECK (true);
+CREATE POLICY update_own_comment ON comments FOR UPDATE USING (auth.uid() = (SELECT user_id FROM ratings WHERE id = rating_id));
+CREATE POLICY delete_own_comment ON comments FOR DELETE USING (auth.uid() = (SELECT user_id FROM ratings WHERE id = rating_id));
+CREATE POLICY select_all_comments ON comments FOR SELECT USING (true);
 
-GRANT SELECT ON TABLE library.comments TO PUBLIC;
-GRANT INSERT, UPDATE, DELETE ON TABLE library.comments TO authenticated;
+GRANT SELECT ON TABLE comments TO PUBLIC;
+GRANT INSERT, UPDATE, DELETE ON TABLE comments TO authenticated;
 
 CREATE TYPE reading_status AS ENUM ('Currently Reading', 'Have Read', 'Want to Read');
 
-CREATE TABLE library.reading_statuses (
+CREATE TABLE reading_statuses (
     id serial PRIMARY KEY,
     book_id varchar(20) NOT NULL, 
     user_id uuid NOT NULL REFERENCES auth.users(id),
@@ -58,14 +54,14 @@ CREATE TABLE library.reading_statuses (
     UNIQUE(user_id, book_id)
 );
 
-CREATE INDEX idx_reading_statuses_user_id ON library.reading_statuses(user_id);
+CREATE INDEX idx_reading_statuses_user_id ON reading_statuses(user_id);
 
-ALTER TABLE library.reading_statuses ENABLE ROW LEVEL SECURITY;
+ALTER TABLE reading_statuses ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY insert_status ON library.reading_statuses FOR INSERT TO authenticated WITH CHECK (true);
-CREATE POLICY update_own_status ON library.reading_statuses FOR UPDATE USING (auth.uid() = user_id);
-CREATE POLICY delete_own_status ON library.reading_statuses FOR DELETE USING (auth.uid() = user_id);
-CREATE POLICY select_all_statuses ON library.reading_statuses FOR SELECT USING (true);
+CREATE POLICY insert_status ON reading_statuses FOR INSERT TO authenticated WITH CHECK (true);
+CREATE POLICY update_own_status ON reading_statuses FOR UPDATE USING (auth.uid() = user_id);
+CREATE POLICY delete_own_status ON reading_statuses FOR DELETE USING (auth.uid() = user_id);
+CREATE POLICY select_all_statuses ON reading_statuses FOR SELECT USING (true);
 
-GRANT SELECT ON TABLE library.reading_statuses TO PUBLIC;
-GRANT INSERT, UPDATE, DELETE ON TABLE library.reading_statuses TO authenticated;
+GRANT SELECT ON TABLE reading_statuses TO PUBLIC;
+GRANT INSERT, UPDATE, DELETE ON TABLE reading_statuses TO authenticated;
