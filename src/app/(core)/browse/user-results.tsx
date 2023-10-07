86738a299b354bd57cc23e295b0d7602f79601a8
Maksymilian Grabka <contact@mgrabka.com>
2023-10-07 21:00:20 +0200

    feat(browse): improve UX and responsiveness

--- src/app/(core)/browse/user-results.tsx
+++ src/app/(core)/browse/user-results.tsx
@@ -2,15 +2,34 @@ import Link from "next/link"
 import { UserProfile } from "@/types"
 import Avatar from "boring-avatars"
 import { MoveRight } from "lucide-react"
+import { useMediaQuery } from "react-responsive"
 
 import { cn } from "@/lib/utils"
 import { Button, buttonVariants } from "@/components/ui/button"
 
 const UserResults = ({ users }: { users: UserProfile[] }) => {
+  const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 1024px)" })
+  const isMediumScreen = useMediaQuery({
+    query: "(min-width: 640px) and (max-width: 1024px)",
+  })
+
+  let displayLimit
+
+  if (isDesktopOrLaptop) {
+    displayLimit = 8
+  } else if (isMediumScreen) {
+    displayLimit = 6
+  } else {
+    displayLimit = 4
+  }
+
+  const displayedUsersCount =
+    users.length > displayLimit ? displayLimit - 1 : displayLimit
+
   return (
     <div>
       <ul className="flex justify-start gap-1 overflow-x-auto whitespace-nowrap">
-        {users.slice(0, users.length > 8 ? 7 : 8).map((user) => (
+        {users.slice(0, displayedUsersCount).map((user) => (
           <li key={user.user_id}>
             <Link
               href={`/user/${user.username}`}
@@ -29,12 +48,12 @@ const UserResults = ({ users }: { users: UserProfile[] }) => {
             </Link>
           </li>
         ))}
-        {users.length > 8 && (
+        {users.length > displayLimit && (
           <li>
             <Button
               onClick={() => console.log("clicked")}
               variant="ghost"
-              className="flex h-[140px] w-[100px]  flex-col items-center justify-start text-muted-foreground hover:text-primary"
+              className="flex h-[140px] w-[100px] flex-col items-center justify-start text-muted-foreground hover:text-primary"
             >
               <div className="flex h-[64px] items-center justify-center">
                 <MoveRight size={32} />
